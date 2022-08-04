d1d9cd7cc3049786d158851bf82658bfedec322e
whoshayyan <hayanjaber6@gmail.com>
2022-08-04 21:26:16 +0300

    bilal

--- routes/api.php
+++ routes/api.php
@@ -32,8 +32,8 @@ use Illuminate\Support\Facades\Route;
 });*/
 Route::get('/home',[ProductController::class,"index"] );
 Route::middleware('auth:api')->put('users/{id}', [UserController::class,"update"]);
-Route::get('users', [UserController::class,"index"]);
-Route::get('users/{id}', [UserController::class,"show"]);
+Route::get('users', [UserController::class,"index"]);//->>>>>>>>>>bilal
+Route::get('users/{id}', [UserController::class,"show"]);//->>>>>>>>>>>>>>bilal
 Route::post('/store', [UserController::class, "create"]);
 Route::get('/category', [CategoryController::class, "index"]);
 Route::post('/login', [UserController::class, "login"]);

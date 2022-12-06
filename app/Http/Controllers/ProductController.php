2652a2bb19c0310c6de97e64c17d7dd671b621d2
whoshayyan <94853057+whoshayyan@users.noreply.github.com>
2022-12-06 17:08:32 +0300

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -30,14 +30,6 @@ class ProductController extends Controller
         $product = Product::where('user_id',$request->user_id);
         return $product->with('user')->get();
     }
-   /* 'name',
-        'price',
-        'description',
-        'image_url',
-        'quantity',
-        'category_id',
-        'user_id'
-* */
    public function homepageindex(){
     $product = Product::with(['reviews']);
     return $product->get();
@@ -69,7 +61,7 @@ class ProductController extends Controller
             $image_url = 'image_url'.time().'.'.$request->image_url->extension();
             $request->image_url->move(public_path('uploads/product_images'),$image_url);
 
-        // Storage::disk('public')->putFileAs('uploads/product_images/',$request->image_url, $image_url);
+        
 
 
 
@@ -93,37 +85,12 @@ class ProductController extends Controller
                 'data'=>$product,
                 'image_url'=>$url
             ],200);
- // $request->image_url->move(public_path('/uploads/product_images'),$image_url);
-      // $image_url = time().'.'.$request->image_url->extension();
-     // $request->image_url->move(public_path('/uploads/product_images'));
-     //$image_url = $request->file('image')->move(public_path('/uploads/product_images'),$request->file('image')->getClientOriginalName().".".$request->file('image')->getClientOriginalExtension());
-
-              // $url= Storage::get('\public\product_images\{{$image_url}}');
-     //$url = Pubi::url('image_url');
-   /* $product = new Product($request->all());
-     // return "dd";
-     $product->
-    // $id = optional(Auth::user())->id;
-      if ($request->hasFile('img_url')) {
-        $file = $request->file('img_url');
-        $file_extension = $file->getClientOriginalName();
-        $destination_path = public_path() . '/folder/images/';
-        $filename = $file_extension;
-        $request->file('image')->move($destination_path, $filename);
-        $input['image'] = $filename;
-       /* $path = $file->store('public/files');
-        $name = $file->getClientOriginalName();*/
 
 
 
 
 
-
-}                                                               /**
-* if(Auth::user()->id = user->id() && name already exists )
-*/
-/* $input = $request->all();
-$input['user_id'] = auth()->user()->id;*/
+}                                                               
 
 
 
@@ -199,30 +166,12 @@ $input['user_id'] = auth()->user()->id;*/
             'data'=>$result,
         ],200);
 
-       /* $category_id = $request->query('category_id');
-        $price_from = $request->query('price_from');
-        $price_to = $request->query('price_to');
-
-        $product = Product::query();
-        if($category_id){
-            $product->where('category_id', $category_id);
-        }
-        if($price_from){
-            $product->where('price', '>=' , $price_from);
-        }
-        if($price_to){
-            $product->where('price', '=<' , $price_to);
-        }
-        $result = $product->get();*/
+     
 
 
 
     }
-   /* public function showByUser(Request $request,$id){
-
-        return Product::where('user_id','=',$id);
-
-    }*/
+  
 
     public function showByUser(Request $request)
 {
@@ -256,16 +205,7 @@ $input['user_id'] = auth()->user()->id;*/
         ],200);
 
 
-      /*  if($request->sortOrder && in_array($request->sortOrder,['asc','desc'])){
-            $sortOrder=$request->sortOrder;
-        }
-        else{
-            $sortOrder='desc';
-        }
-        $result = $product->orderBY('price',$sortOrder);
-        return response()->json([
-            'data'=>$result,
-        ],200);*/
+   
         return $product;
 
 
@@ -361,155 +301,7 @@ $input['user_id'] = auth()->user()->id;*/
                 'message'=>'Forrbiedn'
             ],403);
         }
-
-
-
-
-
-
-
-
-/*
-       /* if($request->hasFile('iamge_url')){
-            if($product->image_url){
-                $old_path=public_path().'storage/product_images/'
-                        .$product->image_url;
-                if(File::exists($old_path)){
-                    File::delete($old_path);
-                }
-            }
-            if($request->hasFile('image_url')){
-            $image_url = 'image_url'.time().'.'.$request->image_url->extension();
-            $request->image_url->move(public_path('storage/product_images'),$image_url);
-            }
-
-
-        }
-        else{
-            $image_url=$product->image_url;
-        }*/
-    /*    $product = $request->product_id;
-        $name = $request->name;
-        $price = $request->price;
-        $description = $request->description;
-        $quantity = $request->quantity;
-        $category_id = $request->category_id;
-
-
-
-      /*  $updated = TICKET_TRACKER::where('id', $ticket_row_id)
-            ->update(
-                ['assigned_to_id' => $assigned_to_id],
-                ['team_id' => $team_id],
-                ['resolve_date' => $resolve_date],
-                ['status_id' => $status_id],
-                ['description' => $description]
-            );*/
-      /*  $updated = Product::where('id',$product)
-                ->update([
-                    'name' => $name ,
-                    'price' => $price ,
-                    'description' =>$description,
-                    'quantity'=> $quantity,
-                    'category_id' => $category_id,
-
-        ]);
-
-
-        return response()->json([
-            'message'=>'Product has been updated!',
-            'data'=>$updated
-
-        ],200);*/
-
-
-
-
-        /*$product = Product::find($product);
-       $product->update([
-            $product->name = $request->name,
-            $product->description = $request->description,
-            $product->price = $request->price,
-            $product->quantity = $request->quantity,
-            $product->category_id = $request->category_id,
-        ]);
-
-
-        if ($request->has('image_url')) {
-            $image_url = $request->file('image');
-            $filename = $image_url->getClientOriginalName();
-            $image_url->move(public_path('storage/product_images'), $filename);
-            $product->image_url = $request->file('image_url')->getClientOriginalName();
-
-    }
-
-
-            $product->save();
-            return $product;
-*/
-       /*
-            $product->update([
-                'name' => $request->name,
-
-            ]);;
-            return $product;*/
-
-
-          /*  $validator = Validator::make($request->all(),[
-         /*       'name' => 'nullable|string|max:60' ,
-                'price' => 'nullable|int|min:1' ,
-                'description' =>'nullable|string',
-                'image_url'=> 'nullable|mimes:jpg,jpeg,png,doc,docx,pdf,txt,csv|max:2048',
-                'quantity'=> 'nullable|int|max:60',
-                'category_id' => 'nullable',
-            ]);
-
-            if ($validator->fails()) {
-                return response()->json([
-                    'message'=>'Validation fails',
-                    'errors'=>$validator->errors()
-                ],422);
-            }
-
-
-
-            if($request->hasFile('image_url')){
-                if($product->image_url){
-                    $old_path=storage_path().'app/public/profile_images/'
-                            .$product->image_url;
-                    if(File::exists($old_path)){
-                        File::delete($old_path);
-                    }
-                }
-                if($request->hasFile('image_url')){
-                $image_url = 'image_url'.time().'.'.$request->image_url->extension();
-                $request->image_url->move(storage_path('app/public/product_images'),$image_url);
-                }
-
-
-            }
-            else{
-                $image_url=$product->image_url;
-            }
-
-            $product->update([
-                'name' => $request->name ,
-                'price' => $request->price ,
-                'description' =>$request->description,
-                'image_url'=> $image_url,
-                'quantity'=> $request->quantity,
-                'category_id' => $request->category_id,
-            ]);
-
-            return response()->json([
-                'message'=>'Profile has been updated!',
-
-            ],200);
-        }*/
-
-
-
-
+   
 
     }
     public function updatefromMoblie(Request $request, Product $product)
@@ -579,10 +371,3 @@ $input['user_id'] = auth()->user()->id;*/
 
 
 
-/*\n<?php\n\nnamespace App\\Http\\Livewire;\n\nuse App\\Models\\Product;\nuse Livewire\\Component;\nuse Livewire\\WithPagination;\nuse Cart;\nuse App\\Models\\Category;\n\nclass SearchComponent extends Component\n{\n    public $sorting;\n    public $pagesize;\n    public $search;\n    public $product_cat;\n    public $product_cat_id;\n\n    public function mount()\n    {\n        $this->sorting = \"default\";\n        $this->pagesize = 12;\n        $this->fill(request()->only('search','product_cat','product_cat_id'));\n    }\n\n    public function store($product_id,$product_name,$product_price)\n    {\n        Cart::add($product_id,$product_name,1,$product_price)->associate('App\\Models\\Product');\n        session()->flash('success_message','Item added in Cart');\n        return redirect()->route('product.cart');\n    }\n\n    use WithPagination;\n    public function render()\n    {  \n        if($this->sorting=='date')   \n        {\n
-    $products = Product::where('name','like','%'.$this->search .'%')->where('category_id','like','%'.$this->product_cat_id.'%')->orderBy('created_at','DESC')->paginate($this->pagesize);  \n        }\n
-        else if($this->sorting==\"price\")\n        {\n            $products = Product::where('name','like','%'.$this->search .'%')->where('category_id','like','%'.$this->product_cat_id.'%')->orderBy('regular_price','ASC')->paginate($this->pagesize); \n        }\n
-      else if($this->sorting==\"price-desc\")\n        {\n            $products = Product::where('name','like','%'.$this->search .'%')->where('category_id','like','%'.$this->product_cat_id.'%')->orderBy('regular_price','DESC')->paginate($this->pagesize); \n        }\n
-        else{\n            $products = Product::where('name','like','%'.$this->search .'%')->where('category_id','like','%'.$this->product_cat_id.'%')->paginate($this->pagesize);  \n        }   \n        \n
-      $categories = Category::all();\n        \n        return view('livewire.search-component',['products'=> $products,'categories'=>$categories])->layout(\"layouts.base\");\n    }\n}\n
- */
